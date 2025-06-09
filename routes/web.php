e230e74a6d7105b7fee14f669a377414b1f526bb
KalidDiscua33 <kaliddiscua@gmail.com>
2025-06-08 20:37:30 -0600

    Update web.php
    mejora de las rutas

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@ use App\Http\Controllers\MedicoController;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Aquí registras las rutas web de tu aplicación.
 |
 */
 
@@ -21,20 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Rutas para médicos (las que tenías en el conflicto, sin eliminar)
-Route::get('/medicos/crear', [MedicoController::class, 'create'])->name('medicos.create');
-
-Route::post('/medicos', [MedicoController::class, 'store'])->name('medicos.store');
-
-Route::get('/medicos', [MedicoController::class, 'index'])->name('medicos.index');
-
-Route::get('/medicos/ver', [MedicoController::class, 'show'])->name('medicos.show');
-
-Route::get('/medicos/{id}/edit', [MedicoController::class, 'edit'])->name('medicos.edit');
-
-Route::put('/medicos/{id}', [MedicoController::class, 'update'])->name('medicos.update');
-
-Route::resource('medicos', MedicoController::class);  // ojo que esto ya genera rutas para index, create, store, show, edit, update, destroy
+// Rutas para médicos: usamos solo Route::resource para CRUD completo
+Route::resource('medicos', MedicoController::class);
 
 // Login
 Route::get('/login', function () {
@@ -59,24 +45,12 @@ Route::post('/logout', function () {
     return redirect()->route('login');
 })->name('logout');
 
-// Solo esta ruta protegida por sesión
+// Ruta protegida por sesión
 Route::middleware('check.sesion')->get('/empleados/visualizacion', [EmpleadoController::class, 'visualizacion'])->name('empleados.visualizacion');
 
 // Rutas públicas de empleados
-Route::get('/empleados', [EmpleadoController::class, 'index'])->name('empleados.index');
-Route::get('/empleados/create', [EmpleadoController::class, 'create'])->name('empleados.create');
-Route::post('/empleados', [EmpleadoController::class, 'store'])->name('empleados.store');
-Route::get('/empleados/{empleado}', [EmpleadoController::class, 'show'])->name('empleados.show');
-Route::get('/empleados/{empleado}/edit', [EmpleadoController::class, 'edit'])->name('empleados.edit');
-Route::put('/empleados/{empleado}', [EmpleadoController::class, 'update'])->name('empleados.update');
-Route::delete('/empleados/{empleado}', [EmpleadoController::class, 'destroy'])->name('empleados.destroy');
+Route::resource('empleados', EmpleadoController::class);
 
-// Rutas públicas de puestos (incluye show)
-Route::get('/puestos', [PuestoController::class, 'index'])->name('puestos.index');
-Route::get('/puestos/create', [PuestoController::class, 'create'])->name('puestos.create');
-Route::post('/puestos', [PuestoController::class, 'store'])->name('puestos.store');
-Route::get('/puestos/{puesto}', [PuestoController::class, 'show'])->name('puestos.show');
-Route::get('/puestos/{puesto}/edit', [PuestoController::class, 'edit'])->name('puestos.edit');
-Route::put('/puestos/{puesto}', [PuestoController::class, 'update'])->name('puestos.update');
-Route::delete('/puestos/{puesto}', [PuestoController::class, 'destroy'])->name('puestos.destroy');
+// Rutas públicas de puestos
+Route::resource('puestos', PuestoController::class);
 
