b87649fe372a4b84c07ad80b43b665ab2b3b36b1
ElsyKhgj <elsycruz@unah.hn>
2025-06-11 11:16:05 -0600

    Corrigiendo y actualizando errores

--- routes/web.php
+++ routes/web.php
@@ -5,32 +5,6 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\PuestoController;
 use App\Http\Controllers\EmpleadoController;
 
-// Login
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
-Route::post('/login', function (Request $request) {
-    if ($request->input('clave') === '1234') {
-        session([
-            'autenticado' => true,
-            'usuario_nombre' => 'Administrador',
-            'permisos' => ['ver_empleados', 'crear_empleados', 'editar_empleados', 'eliminar_empleados']
-        ]);
-        return redirect()->route('empleados.visualizacion');
-    }
-
-    return back()->withErrors(['clave' => 'Clave incorrecta.']);
-})->name('login.submit');
-
-Route::post('/logout', function () {
-    session()->flush();
-    return redirect()->route('login');
-})->name('logout');
-
-// Solo esta ruta protegida por sesión
-Route::middleware('check.sesion')->get('/empleados/visualizacion', [EmpleadoController::class, 'visualizacion'])->name('empleados.visualizacion');
-
 // Rutas públicas de empleados
 Route::get('/empleados', [EmpleadoController::class, 'index'])->name('empleados.index');
 Route::get('/empleados/create', [EmpleadoController::class, 'create'])->name('empleados.create');
@@ -40,7 +14,7 @@ Route::get('/empleados/{empleado}/edit', [EmpleadoController::class, 'edit'])->n
 Route::put('/empleados/{empleado}', [EmpleadoController::class, 'update'])->name('empleados.update');
 Route::delete('/empleados/{empleado}', [EmpleadoController::class, 'destroy'])->name('empleados.destroy');
 
-// Rutas públicas de puestos (incluye show)
+// Rutas públicas de puestos
 Route::get('/puestos', [PuestoController::class, 'index'])->name('puestos.index');
 Route::get('/puestos/create', [PuestoController::class, 'create'])->name('puestos.create');
 Route::post('/puestos', [PuestoController::class, 'store'])->name('puestos.store');
