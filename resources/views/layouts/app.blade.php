ec4cb31c4acab52e067693cd80645d2c8f1f0115
loani <tu_correoejemplo.com>
2025-06-12 01:10:24 -0600

    Recupero archivos perdidos: create.blade.php, controlador, modelo, migración y layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"/>
-  
     
     {{-- Estilos personalizados --}}
     <style>
