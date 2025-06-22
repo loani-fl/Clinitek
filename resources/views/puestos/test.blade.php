d464342933f62ee97358c1a77eb46fa12d7d9bff
cindi302 <sosacindi05@gmail.com>
2025-06-21 18:46:27 -0600

    Vaciar rama localmente

--- resources/views/puestos/test.blade.php
+++
@@ -1,6 +0,0 @@
-<h1>Detalles del puesto</h1>
-<p>Código: {{ $puesto->codigo }}</p>
-<p>Nombre: {{ $puesto->nombre }}</p>
-<p>Área: {{ $puesto->area }}</p>
-<p>Sueldo: {{ $puesto->sueldo }}</p>
-<p>Función: {{ $puesto->funcion }}</p>
