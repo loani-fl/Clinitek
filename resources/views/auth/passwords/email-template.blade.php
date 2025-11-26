0bfcdaad7a0d3d91792c5046cc7decc3eb8c44a6
cindi302 <sosacindi05@gmail.com>
2025-11-26 01:12:56 -0600

    Se cambio titulo

--- resources/views/auth/passwords/email-template.blade.php
+++ resources/views/auth/passwords/email-template.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recuperar Contraseña - Grupo Centinela</title>
+    <title>Recuperar Contraseña - Clinica electronica</title>
     <style>
         * {
             margin: 0;
